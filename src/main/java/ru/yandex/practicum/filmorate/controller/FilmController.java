060c4c8b0520282d861fef2a3997a9004a55eb9f
yayayi <evgegnii-homichk@yandex.ru>
2022-12-11 17:52:08 +0300

    feat: added validation for films and users

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.controller;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,6 +19,7 @@ public class FilmController {
 
     @PostMapping
     public Film create(@RequestBody Film film) {
+        validateFilm(film);
         if (films.contains(film)) {
             throw new RuntimeException();
         }
@@ -27,7 +29,26 @@ public class FilmController {
 
     @PutMapping
     public Film update(@RequestBody Film film) {
+        validateFilm(film);
         films.add(film);
         return film;
+    }
+
+    private void validateFilm(Film film) {
+        if (film.getName() == null || film.getName().isEmpty() || film.getName().isBlank()) {
+            throw new RuntimeException("Film's name shouldn't be empty.");
+        }
+
+        if (film.getDescription().length() > 200) {
+            throw new RuntimeException("Film's description shouldn't contain more than 200 symbols.");
+        }
+
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new RuntimeException("Film's release date can't be before 28.12.1895.");
+        }
+
+        if (film.getDuration() < 0) {
+            throw new RuntimeException("Film's duration can't be negative.");
+        }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserContoller.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserContoller.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.controller;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,6 +19,7 @@ public class UserContoller {
 
     @PostMapping
     public User create(@RequestBody User user) {
+        validateUser(user);
         if (users.contains(user)) {
             throw new RuntimeException();
         }
@@ -27,7 +29,28 @@ public class UserContoller {
 
     @PutMapping
     public User update(@RequestBody User user) {
+        validateUser(user);
         users.add(user);
         return user;
+    }
+
+    private void validateUser(User user) {
+        if (user.getEmail() == null || user.getEmail().isEmpty() || user.getEmail().isBlank()) {
+            throw new RuntimeException("User's email shouldn't be empty.");
+        }
+        if (!user.getEmail().contains("@")) {
+            throw new RuntimeException("Wrong user's email input.");
+        }
+
+        if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().isBlank()) {
+            throw new RuntimeException("User's login shouldn't be empty.");
+        }
+        if (user.getLogin().contains(" ")) {
+            throw new RuntimeException("User's login shouldn't contain spaces.");
+        }
+
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new RuntimeException("User's birthday can't be in the future.");
+        }
     }
 }
