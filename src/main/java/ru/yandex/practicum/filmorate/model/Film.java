864b8980c8b3060ae7a4e34ce87209bdd83585b6
yayayi <evgegnii-homichk@yandex.ru>
2022-12-12 19:40:19 +0300

    test: added tests for films

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,14 +1,16 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class Film {
-    private final int id;
+    private transient final int id;
     private final String name;
-    private final String description;
-    private final LocalDate releaseDate;
-    private final int duration;
+    private transient final String description;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private transient final LocalDate releaseDate;
+    private transient final int duration;
 }
