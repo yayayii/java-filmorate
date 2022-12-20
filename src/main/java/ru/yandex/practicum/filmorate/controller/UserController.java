51080ec202502fdf1590c2f3d874368424ba263f
yayayi <evgegnii-homichk@yandex.ru>
2022-12-20 23:37:26 +0300

    refactor: refactored controllers for working with storages

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,66 +1,39 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.UserDoesntExistException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import javax.validation.Valid;
-import java.util.HashSet;
 import java.util.Set;
 
-@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private static int id = 0;
-    private final Set<User> users = new HashSet<>();
+    private final UserStorage userStorage;
+
+    public UserController() {
+        userStorage = new InMemoryUserStorage();
+    }
 
     @GetMapping
-    public Set<User> findAll() {
-        return users;
+    public Set<User> getUsers() {
+        return userStorage.getUsers();
     }
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
-        validateUser(user);
-        user.setId(++id);
-        users.add(user);
-        log.info("User \"" + user.getLogin() + "\" was added.");
-        return user;
+        return userStorage.addUser(user);
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User user) {
-        validateUser(user);
-        if (!users.contains(user)) {
-            RuntimeException exception = new UserDoesntExistException("User \"" + user.getLogin() + "\" doesn't exists.");
-            log.warn(exception.getMessage());
-            throw exception;
-        }
-        users.remove(user);
-        users.add(user);
-        log.info("User \"" + user.getLogin() + "\" was updated.");
-        return user;
+        return userStorage.updateUser(user);
     }
 
     @DeleteMapping
     public void clear() {
-        id = 0;
-        users.clear();
-        log.info("User set was cleared.");
-    }
-
-    private void validateUser(User user) {
-        if (user.getLogin().contains(" ")) {
-            RuntimeException exception = new ValidationException("User's login shouldn't contain spaces.");
-            log.warn(exception.getMessage());
-            throw exception;
-        }
-
-        if (user.getName() == null || user.getName().isEmpty() || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
+        userStorage.clearUserCollection();
     }
 }
