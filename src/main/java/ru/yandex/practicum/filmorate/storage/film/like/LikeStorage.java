c3daebfc1f116ff3a76c897471ea41d8eb439d84
yayayi <evgegnii-homichk@yandex.ru>
2023-01-27 17:33:47 +0300

    feat: added storage for likes

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/like/LikeStorage.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.storage.film.like;
+
+import ru.yandex.practicum.filmorate.model.film.Film;
+
+import java.util.Set;
+
+public interface LikeStorage {
+    //create
+    void addLike(int filmId, int userId);
+    //read
+    Set<Integer> getLikedUsersIds(int filmId);
+    Set<Film> getPopularFilms(int count);
+    //update
+    //delete
+    void removeLike(int filmId, int userId);
+}
